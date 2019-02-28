5d24864fb8df4463375307a288e72b8381a78317
Andres Salinas <andressalinas@Eduardos-MacBook-Air.local>
2019-02-28 14:34:50 -0500

    Added google maps, needs fix

--- www/js/controllers/map.js
+++ www/js/controllers/map.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.controller('MapCtrl', function($scope, $ionicHistory) {
+.controller('MapCtrl', function($scope, $ionicHistory, $cordovaGeolocation) {
     $scope.verifyMap = function() {
         console.log('verified');
     }
@@ -10,4 +10,35 @@ angular.module('starter.controllers')
     $scope.goBack = function(){
         $ionicHistory.goBack();
     }
+
+    var onSuccess = function(position) {
+        console.log('Latitude: ' + position.coords.latitude + '\n' +
+                    'Longitude: ' + position.coords.longitude + '\n' +
+                    'Altitude: ' + position.coords.altitude + '\n');
+    }
+
+    function onError(error) {
+        console.log('code: ' + error.code + '\n' +
+                    'message: ' + error.message + '\n');
+    }
+
+    $scope.getGPSlocation = function() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+
+    var options = {timeout: 1000, enableHighAccuracy: true};
+
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
+        var latLng = new google.maps.latLng(position.coords.latitude, position.coords.longitude);
+
+        var mapOptions = {
+            center: latLng,
+            zoom: 15,
+            mapTypeId: google.maps.mapTypeId.ROADMAP
+        };
+
+        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    }, function(error) {
+        console.log("Could not get location");
+    });
 });
